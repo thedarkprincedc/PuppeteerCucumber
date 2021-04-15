65019ebb0567ffbf9e96ca88762e71e2ed6a7712
Brett Mosley <brettmosley@hotmail.com>
2021-04-15 02:51:45 +0000

    Added youtube.js

---
+++ pages/youtube.js
@@ -0,0 +1,16 @@
+const {Given} = require('@cucumber/cucumber')
+const assert = require('assert').strict;
+//const puppeteer = require('puppeteer');
+Given('I have {int} cucumbers in my belly', async function (cucumberCount) {
+  
+
+    //(async () => {
+      // const browser = await puppeteer.launch();
+      // const page = await browser.newPage();
+      // await page.goto('https://google.com');
+      // await page.screenshot({ path: 'example.png' });
+
+      // await browser.close();
+   /// })();
+  //assert.equal(42, cucumberCount)
+});
